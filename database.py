bcb349939640047351dd3f06cffdd8cc55773648
sumpumm <samarpan69.piya@gmail.com>
2026-04-27 15:00:44 +0545

    database initialization and connection cempleted

---
+++ database.py
@@ -0,0 +1,18 @@
+from langchain_community.utilities.sql_database import SQLDatabase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_user = os.getenv("db_user")
+db_password = os.getenv("db_password")
+db_host = os.getenv("db_host")
+db_name = os.getenv("db_name")
+db_port = os.getenv("db_port")
+
+connection_string = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+db= SQLDatabase.from_uri(connection_string)
+
+print(db.dialect)
+print(db.get_usable_table_names())
